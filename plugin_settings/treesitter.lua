2abf4b9c690f19a54e3b60a6691a15d3d757dfca
max397574 <max.heinzer397@gmx.ch>
2021-07-18 12:13:08 +0200

    redid indent and added new module

--- plugin_settings/treesitter.lua
+++ plugin_settings/treesitter.lua
@@ -11,126 +11,134 @@ true)
 -- so $VIMRUNTIME/syntax/hitest.vim to see colors
 
 require'nvim-treesitter.configs'.setup {
-    ensure_installed = 'maintained',
-    highlight = {
-        enable = true,
-        --disable = { "c", "rust" },  -- list of language that will be disabled
-        custom_captures = {
-        -- Highlight the @foo.bar capture group with the "Identifier" highlight group.
-        --["foo.bar"] = "Identifier",
-          ["variable"] = "YellowVariable",
-          ["keyword.operator"] = "RedConditional",
-	  ["conditional"] = "RedConditional",
-          ["number"] = "BlueNumber",
-          ["float"] = "BlueNumber",
-          ["operator"] = "BlueOperator",
-          ["keyword"] = "ItalicRed",
-          ["string"] = "GreenString",
-	  ["field"] = "YellowVariable",
-        },
+  ensure_installed = 'maintained',
+  highlight = {
+    enable = true,
+    --disable = { "c", "rust" },  -- list of language that will be disabled
+    custom_captures = {
+      -- Highlight the @foo.bar capture group with the "Identifier" highlight group.
+      --["foo.bar"] = "Identifier",
+      ["variable"] = "YellowVariable",
+      ["keyword.operator"] = "RedConditional",
+      ["conditional"] = "RedConditional",
+      ["number"] = "BlueNumber",
+      ["float"] = "BlueNumber",
+      ["operator"] = "BlueOperator",
+      ["keyword"] = "ItalicRed",
+      ["string"] = "GreenString",
+      ["field"] = "YellowVariable",
     },
-    refactor = {
-        highlight_definitions = { enable = true },
-        highlight_current_scope = { enable = false },
-        smart_rename = {
-            enable = true,
-            keymaps = {
-                smart_rename = "grr",
-            },
-        },
-        navigation = {
-            enable = true,
-            keymaps = {
-                goto_definition = "gnd",
-                list_definitions = "gnD",
-                list_definitions_toc = "gO",
-                goto_next_usage = "<leader>n",
-                goto_previous_usage = "<leader>p",
-            },
-        },
+  },
+  refactor = {
+    highlight_definitions = { enable = true },
+    highlight_current_scope = { enable = false },
+    smart_rename = {
+      enable = true,
+      keymaps = {
+	smart_rename = "grr",
+      },
     },
-    textobjects = {
-        select = {
-            enable = false,
-            keymaps = {
-                -- You can use the capture groups defined in textobjects.scm
-		-- https://github.com/nvim-treesitter/nvim-treesitter-textobjects/blob/master/queries/python/textobjects.scm
-                ["af"] = "@function.outer",
-                ["if"] = "@function.inner",
-                ["ac"] = "@class.outer",
-                ["ic"] = "@class.inner",
+    navigation = {
+      enable = true,
+      keymaps = {
+	goto_definition = "gnd",
+	list_definitions = "gnD",
+	list_definitions_toc = "gO",
+	goto_next_usage = "<leader>n",
+	goto_previous_usage = "<leader>p",
+      },
+    },
+  },
+  textobjects = {
+    lsp_interop = {
+      enable = true,
+      border = 'none',
+      peek_definition_code = {
+	["df"] = "@function.outer",
+	["dF"] = "@class.outer",
+      },
+    },
+    select = {
+      enable = false,
+      keymaps = {
+	-- You can use the capture groups defined in textobjects.scm
+	-- https://github.com/nvim-treesitter/nvim-treesitter-textobjects/blob/master/queries/python/textobjects.scm
+	["af"] = "@function.outer",
+	["if"] = "@function.inner",
+	["ac"] = "@class.outer",
+	["ic"] = "@class.inner",
 
-                -- Or you can define your own textobjects like this
-                --["iF"] = {
-                --python = "(function_definition) @function",
-                --cpp = "(function_definition) @function",
-                --c = "(function_definition) @function",
-                --java = "(method_declaration) @function",
-            },
-        },
-        swap = {
-            enable = true,
-            swap_next = {
-                ["<leader>a"] = "@parameter.inner",
-            },
-            swap_previous = {
-                ["<leader>A"] = "@parameter.inner",
-            },
-        },
-        move = {
-            enable = false,
-            set_jumps = true, -- whether to set jumps in the jumplist
-            goto_next_start = {
-                ["]m"] = "@function.outer",
-                ["]]"] = "@class.outer",
-            },
-            goto_next_end = {
-                ["]M"] = "@function.outer",
-                ["]["] = "@class.outer",
-            },
-            goto_previous_start = {
-                ["[m"] = "@function.outer",
-                ["[["] = "@class.outer",
-            },
-            goto_previous_end = {
-                ["[M"] = "@function.outer",
-                ["[]"] = "@class.outer",
-            },
-        },
+	-- Or you can define your own textobjects like this
+	--["iF"] = {
+	--python = "(function_definition) @function",
+	--cpp = "(function_definition) @function",
+	--c = "(function_definition) @function",
+	--java = "(method_declaration) @function",
+      },
     },
-    playground = {
-        enable = true,
-        disable = {},
-        updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
-        persist_queries = false, -- Whether the query persists across vim sessions
-        keybindings = {
-            toggle_query_editor = 'o',
-            toggle_hl_groups = 'i',
-            toggle_injected_languages = 't',
-            toggle_anonymous_nodes = 'a',
-            toggle_language_display = 'I',
-            focus_language = 'f',
-            unfocus_language = 'F',
-            update = 'R',
-            goto_node = '<CR>',
-            show_help = '?',
-        },
+    swap = {
+      enable = true,
+      swap_next = {
+	["<leader>a"] = "@parameter.inner",
+      },
+      swap_previous = {
+	["<leader>A"] = "@parameter.inner",
+      },
     },
-    query_linter = {
-        enable = true,
-        use_virtual_text = true,
-        lint_events = {"BufWrite", "CursorHold"},
+    move = {
+      enable = false,
+      set_jumps = true, -- whether to set jumps in the jumplist
+      goto_next_start = {
+	["]m"] = "@function.outer",
+	["]]"] = "@class.outer",
+      },
+      goto_next_end = {
+	["]M"] = "@function.outer",
+	["]["] = "@class.outer",
+      },
+      goto_previous_start = {
+	["[m"] = "@function.outer",
+	["[["] = "@class.outer",
+      },
+      goto_previous_end = {
+	["[M"] = "@function.outer",
+	["[]"] = "@class.outer",
+      },
     },
-    incremental_selection = {
-        enable = false,
-        keymaps = {
-            init_selection = "gnn",
-            node_incremental = "grn",
-            scope_incremental = "grc",
-            node_decremental = "grm",
-        },
+  },
+  playground = {
+    enable = true,
+    disable = {},
+    updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
+    persist_queries = false, -- Whether the query persists across vim sessions
+    keybindings = {
+      toggle_query_editor = 'o',
+      toggle_hl_groups = 'i',
+      toggle_injected_languages = 't',
+      toggle_anonymous_nodes = 'a',
+      toggle_language_display = 'I',
+      focus_language = 'f',
+      unfocus_language = 'F',
+      update = 'R',
+      goto_node = '<CR>',
+      show_help = '?',
     },
-    indent = {
-        enable = false
-    }
+  },
+  query_linter = {
+    enable = true,
+    use_virtual_text = true,
+    lint_events = {"BufWrite", "CursorHold"},
+  },
+  incremental_selection = {
+    enable = false,
+    keymaps = {
+      init_selection = "gnn",
+      node_incremental = "grn",
+      scope_incremental = "grc",
+      node_decremental = "grm",
+    },
+  },
+  indent = {
+    enable = true
   }
+}
