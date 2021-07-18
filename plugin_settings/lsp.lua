8cc0c8c106dc771ac0ee2a1239713a7002106a70
max397574 <max.heinzer397@gmx.ch>
2021-07-18 17:09:03 +0200

    fixed formatting

--- plugin_settings/lsp.lua
+++ plugin_settings/lsp.lua
@@ -9,23 +9,28 @@ Plug 'folke/lua-dev.nvim'
 call plug#end()
 ]],
 true)
+
 local luadev = require("lua-dev").setup({
   --lspconfig = {
     --cmd = {"lua-language-server"}
   --},
 })
+
 require("lsp-colors").setup({
   Error = "#db4b4b",
   Warning = "#e0af68",
   Information = "#0db9d7",
   Hint = "#10B981"
 })
+
 require'lspconfig'.pyright.setup{
   flags = {
     debounce_text_changes = 500,
   }
 }
+
 require'lspconfig'.sumneko_lua.setup(luadev)
+
 require'lspinstall'.setup() -- important
 
 local servers = require'lspinstall'.installed_servers()
