ccf9c699ad541017441113ffc1060c7f07861bc7
max397574 <max.heinzer397@gmx.ch>
2021-07-18 21:04:21 +0200

    removed lsp-colors plugin

--- plugin_settings/lsp.lua
+++ plugin_settings/lsp.lua
@@ -3,7 +3,6 @@ vim.api.nvim_exec(
 call plug#begin('~/.vim/plugged')
 Plug 'neovim/nvim-lspconfig'
 Plug 'kabouzeid/nvim-lspinstall'
-Plug 'folke/lsp-colors.nvim'
 Plug 'folke/trouble.nvim'
 Plug 'folke/lua-dev.nvim'
 call plug#end()
@@ -14,13 +13,6 @@ local luadev = require("lua-dev").setup({
   --lspconfig = {
     --cmd = {"lua-language-server"}
   --},
-})
-
-require("lsp-colors").setup({
-  Error = "#db4b4b",
-  Warning = "#e0af68",
-  Information = "#0db9d7",
-  Hint = "#10B981"
 })
 
 require'lspconfig'.pyright.setup{
