a82f2bfdfe8372b650445f30d19bc42b07438c25
max397574 <max.heinzer397@gmx.ch>
2021-07-18 17:44:56 +0200

    added treesitter

--- plugin_settings/telescope.lua
+++ plugin_settings/telescope.lua
@@ -1,6 +1,7 @@
 vim.api.nvim_exec(
 [[
 call plug#begin('~/.vim/plugged')
+Plug 'nvim-treesitter/nvim-treesitter'
 Plug 'nvim-lua/popup.nvim'
 Plug 'nvim-lua/plenary.nvim'
 Plug 'nvim-telescope/telescope.nvim'
