9fbf8808832802af0bdf737951845356c5d12196
max397574 <max.heinzer397@gmx.ch>
2021-07-21 08:57:06 +0200

    removed formatoptions and autoindent

--- options.lua
+++ options.lua
@@ -25,7 +25,6 @@ opt.foldlevel = 0
 opt.joinspaces = false
 opt.completeopt = "menuone,noselect"
 opt.signcolumn = "yes"
-opt.autoindent = true
 opt.termguicolors = true
 opt.conceallevel = 0
 
@@ -48,16 +47,6 @@ vim.api.nvim_exec([[
 ]],
 true)
 
-opt.formatoptions = opt.formatoptions
-  - "a" -- Auto formatting is BAD.
-  - "t" -- Don't auto format my code. I got linters for that.
-  + "c" -- In general, I like it when comments respect textwidth
-  + "q" -- Allow formatting comments w/ gq
-  - "o" -- O and o, don't continue comments
-  + "r" -- But do continue when pressing enter.
-  + "n" -- Indent past the formatlistpat, not underneath it.
-  + "j" -- Auto-remove comments if possible.
-  - "2"
 
 opt.joinspaces = false
 
