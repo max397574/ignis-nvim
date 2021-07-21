98267cdd15851e3c603c14e5187d9b623f1dbcb8
max397574 <max.heinzer397@gmx.ch>
2021-07-21 14:44:56 +0200

    changed mapping

--- plugin_settings/comment.lua
+++ plugin_settings/comment.lua
@@ -14,7 +14,7 @@ require('nvim_comment').setup({
   -- Should key mappings be created
   create_mappings = true,
   -- Normal mode mapping left hand side
-  line_mapping = "dont use this",
+  line_mapping = "<leader>cc",
   -- Visual/Operator mapping left hand side
-  operator_mapping = "<leader>cc"
+  operator_mapping = "<leader>c"
 })
