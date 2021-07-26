6e33920ec8b7091b57119dcfcc6f9b8f8d0a8b7e
max397574 <max.heinzer397@gmx.ch>
2021-07-26 19:40:40 +0200

    moved plugin source

--- init.lua
+++ init.lua
@@ -1,6 +1,5 @@
 vim.g.mapleader = " "
 
-vim.cmd('source ~/.config/nvim/plugins.vim')
 require("config_files.options")
 require("plugin_settings.colorizer")
 require("plugin_settings.comment")
@@ -13,6 +12,7 @@ require("plugin_settings.trouble")
 require("plugin_settings.twilight")
 require("plugin_settings.which_key")
 require("plugin_settings.compe")
+vim.cmd('source ~/.config/nvim/plugins.vim')
 vim.cmd('source ~/.config/nvim/tablemode.vim')
 vim.cmd('source ~/.config/nvim/mapping.vim')
 vim.cmd('source ~/.config/nvim/autocmds.vim')
