7dd34c8ae9ea5639e553d3a0f410e38d370d3a43
max397574 <max.heinzer397@gmx.ch>
2021-08-04 21:45:56 +0200

    created options

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,7 +1,9 @@
 local map = vim.api.nvim_set_keymap
-local options = { noremap = true }
+local nore_silent = { noremap = true, silent = true }
+local nore = { noremap = true }
+
 -- remove highlighting from search
-map('n', 'nh', ':nohlsearch<CR>', { noremap = true, silent = true})
+map('n', 'nh', ':nohlsearch<CR>', nore_silent)
 -- move visual blocks up and down
-map('v', 'J', ":m '>+1<CR>gv=gv", { noremap = true, silent = true })
-map('v', 'K', ":m '<-2<CR>gv=gv", { noremap = true, silent = true })
+map('v', 'J', ":m '>+1<CR>gv=gv", nore_silent)
+map('v', 'K', ":m '<-2<CR>gv=gv", nore_silent)
