56f567e797322131e34ed2388a75aaafff19a369
max397574 <max.heinzer397@gmx.ch>
2021-08-04 18:55:41 +0200

    moved plugins to packer file

--- init.lua
+++ init.lua
@@ -2,14 +2,14 @@ vim.g.mapleader = " "
 
 require("config_files.options")
 require("plugins")
-require("plugin_settings.comment")
-require("plugin_settings.lsp")
-require("plugin_settings.treesitter")
-require("plugin_settings.trouble")
-require("plugin_settings.which_key")
-vim.cmd("source ~/.config/nvim/plugins.vim")
+vim.cmd("colorscheme gruvbox")
 vim.cmd("source ~/.config/nvim/tablemode.vim")
 vim.cmd("source ~/.config/nvim/mapping.vim")
 require("mappings")
 vim.cmd("source ~/.config/nvim/autocmds.vim")
 vim.cmd("source ~/.config/nvim/highlights.vim")
+
+vim.cmd([[
+  source ~/.config/nvim/plugins/markdown.vim
+  source ~/.config/nvim/plugins/simplefunctions.vim
+]])
