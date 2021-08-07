4215273498ec9aac6147179904f672c297a8d94c
max397574 <max.heinzer397@gmx.ch>
2021-08-07 14:15:43 +0200

    moved mappings to lua file

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -4,6 +4,14 @@ local nore = { noremap = true }
 
 -- remove highlighting from search
 map("n", "nh", ":nohlsearch<CR>", nore_silent)
+-- Telescope
+map("n", "<leader>tcff", "<cmd>Telescope find_files<CR>", nore)
+map("n", "<leader>tcts", "<cmd>Telescope treesitter<CR>", nore)
+map("n", "<leader>tcht", "<cmd>Telescope help_tags<CR>", nore)
+map("n", "<leader>tclg", "<cmd>Telescope live_grep<CR>", nore)
+-- highlight search result and center cursor
+map("n", "n", "nzzzv:call HLNext(0.4)<CR>", nore_silent)
+map("n", "N", "Nzzzv:call HLNext(0.4)<CR>", nore_silent)
 -- move visual blocks up and down
 map("v", "J", ":m '>+1<CR>gv=gv", nore_silent)
 map("v", "K", ":m '<-2<CR>gv=gv", nore_silent)
@@ -38,6 +46,23 @@ map("n", "<leader>mdlt", "MdTaskList", nore_silent)
 map("i", "<leader>mdlu", "<ESC>MdUnorderedList", nore_silent)
 map("i", "<leader>mdlo", "<ESC>MdOrderedList", nore_silent)
 map("i", "<leader>mdlt", "<ESC>MdTaskList", nore_silent)
+map("v", "<leader>mdit", ":call VisualItalic()<CR>", nore_silent)
+map("v", "<leader>mdbd", ":call VisualBold()<CR>", nore_silent)
+-- better undo
+map("i", ",", ",<c-g>u", nore)
+map("i", "!", "!<c-g>u", nore)
+map("i", ".", ".<c-g>u", nore)
+map("i", "?", "?<c-g>u", nore)
+-- Snip Run
+map("n", "<leader>sr", ":SnipRun<CR>", nore_silent)
+map("n", "<leader>sc", ":SnipClose<CR>", nore_silent)
+map("v", "<leader>sr", "<Plug>SnipRun", silent)
+-- insert empty line below/above
+map("n", "<leader>o", "o<ESC>k", nore)
+map("n", "<leader>O", "O<ESC>j", nore)
+-- enter space before/after
+map("n", "<leader>i", "i <ESC>l", nore)
+map("n", "<leader>a", "a <ESC>h", nore)
 -- substitute on visual selection
 map("v", "<leader>s", ":s///g<LEFT><LEFT><LEFT>", nore)
 -- copy to system clipboard
@@ -62,3 +87,9 @@ map("n", "<leader>trld", ":Trouble lsp_workspace_diagnostics<CR>", nore_silent)
 -- help from ts with textobjects
 map("o", "m", ":<C-U>lua require('tsht').nodes()<CR>", { silent = true })
 map("v", "m", ":<C-U>lua require('tsht').nodes()<CR>", nore_silent)
+-- visual multi
+vim.cmd([[let g:VM_maps = {}
+  let g:VM_maps["Add Cursor Down"]    = '<Leader>cd'   " new cursor down
+  let g:VM_maps["Add Cursor Up"]      = '<Leader>cu'   " new cursor up
+  let g:VM_maps['Find Under']         = '<Leader>fu'
+]])
--- mapping.vim
+++ mapping.vim
@@ -1,28 +1,6 @@
-"{{{ Telescope mappings
-nnoremap <leader>tcff <cmd>Telescope find_files<CR>
-nnoremap <leader>tcts <cmd>Telescope treesitter<CR>
-nnoremap <leader>tcht <cmd>Telescope help_tags<CR>
-nnoremap <leader>tclg <cmd>Telescope live_grep<CR>
-"1}}}
-
-"{{{ changes of some keybindings
-"add j and k with count to jumplist
 nnoremap <expr> j (v:count > 1 ? "m'" . v:count : '') . 'j'
 nnoremap <expr> k (v:count > 1 ? "m'" . v:count : '') . 'k'
-" highlight search result and center cursor
-nnoremap <silent> n   nzzzv:call HLNext(0.4)<CR>
-nnoremap <silent> N   Nzzzv:call HLNext(0.4)<CR>
-"Swap commands for visual and visual block mode
-nnoremap v <C-V>
-nnoremap <C-V> v
-xnoremap v <C-V>
-xnoremap <C-V> v
-"}}}
 
-vnoremap <silent> <leader>mdit :call VisualItalic()<CR>
-vnoremap <silent> <leader>mdbd :call VisualBold()<CR>
-
-"{{{ Shortcuts for some commands/keybindings
 " move throught completitions with tab
 inoremap <expr> <Tab> pumvisible() ? "\<C-n>" : "\<Tab>"
 " move lines up and down in visual and normal mode
@@ -30,31 +8,3 @@ inoremap <C-j> <ESC>:m .+1<CR>==i
 inoremap <C-k> <ESC>:m .-2<CR>==i
 nnoremap <silent> <leader>j :m .+1<CR>==
 nnoremap <silent> <leader>k :m .-2<CR>==
-"}}}
-
-let g:VM_maps = {}
-let g:VM_maps["Add Cursor Down"]    = '<Leader>cd'   " new cursor down
-let g:VM_maps["Add Cursor Up"]      = '<Leader>cu'   " new cursor up
-let g:VM_maps['Find Under']         = '<Leader>fu'
-" run snippet
-nnoremap <silent> <leader>sr :SnipRun<CR>
-" close result of snippet run
-nnoremap <silent> <leader>sc :SnipClose<CR>
-" run snippet
-vmap <leader>sr <Plug>SnipRun
-
-"{{{ Insert empty space/lines
-" insert empty line below/above
-nnoremap <leader>o o<ESC>k
-nnoremap <leader>O O<ESC>j
-" enter space before/after
-nnoremap <leader>i i <ESC>l
-nnoremap <leader>a a <ESC>h
-"}}}
-
-"{{{ Better undo
-inoremap , ,<c-g>u
-inoremap ! !<c-g>u
-inoremap . .<c-g>u
-inoremap ? ?<c-g>u
-"1}}}
