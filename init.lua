ae42601da76175585ae4c5d3a2c32f32816de415
max397574 <max.heinzer397@gmx.ch>
2021-08-08 13:20:48 +0200

    added new file and comments

--- init.lua
+++ init.lua
@@ -1,12 +1,19 @@
 vim.g.mapleader = " "
 
 require("config_files.options")
+-- plugins (packer) and settings
 require("plugins")
 vim.cmd("colorscheme gruvbox")
+-- all the mappings
 require("mappings")
+-- the autocommands
 require("autocommands")
+-- some stuff which hasn't been converted to lua yet
 vim.cmd("source ~/.config/nvim/random.vim")
+-- all highlights
 vim.cmd("source ~/.config/nvim/highlights.vim")
+-- file for testing some configurations
+vim.cmd("source ~/.config/nvim/temporary.vim")
 
 vim.cmd([[
   source ~/.config/nvim/plugins/markdown.vim
