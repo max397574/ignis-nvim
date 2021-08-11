5420c3ced55f276796f21fb8fe6593c858f43b41
max397574 <max.heinzer397@gmx.ch>
2021-08-11 11:34:08 +0200

    changed mapping for git status

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -17,7 +17,7 @@ map("n", "<leader>ga", ":Git add %<CR>", nore_silent)
 map("n", "<leader>gc", ":Git commit<CR>", nore_silent)
 map("n", "<leader>gl", ":GV<CR>", nore_silent)
 map("n", "<leader>gp", ":!git push origin<CR>", nore_silent)
-map("n", "<leader>gs", ":Git status<CR>", nore_silent)
+map("n", "<leader>gs", ":G<CR>", nore_silent)
 map("n", "<leader>gd", ":Git diff<CR>", nore_silent)
 -- treesitter playground
 map("n", "<leader>tspg", ":TSPlaygroundToggle<CR>", nore_silent)
