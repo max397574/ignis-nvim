af75649f7c5ddda452944ff59ce918ca33f4230f
max397574 <max.heinzer397@gmx.ch>
2021-08-13 13:43:19 +0200

    higher priority

--- queries/lua/highlights.scm
+++ queries/lua/highlights.scm
@@ -1,4 +1,7 @@
-(function_call
-  (identifier) @require_call
-  (#match? @require_call "require")
+(
+    (function_call
+        (identifier) @require_call
+        (#match? @require_call "require")
+    )
+    (set! "priority" 105)
 )
