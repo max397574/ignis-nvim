00e896a2f4b4e75ba26b85932b92369a283e9b89
max397574 <max.heinzer397@gmx.ch>
2021-08-13 09:36:18 +0200

    initial commit

---
+++ lua/colors/fixed_highlights.lua
@@ -0,0 +1,13 @@
+vim.cmd[[
+  highlight Normal guibg = none
+  highlight SignColumn guibg = none
+  highlight Comment term=bold cterm=italic ctermfg=white gui=italic guifg=white
+  highlight WhiteOnRed ctermbg=red guibg=red ctermfg=white guifg=white
+  highlight ItalicRed term=italic ctermfg=12 gui=italic guifg=#b30b00
+  highlight SignifyLineChange ctermfg=108 ctermbg=none guifg=#FE8019 guibg=none
+  highlight SignifySignChange ctermfg=108 ctermbg=none guifg=#FE8019 guibg=none
+  highlight SignifyLineAdd ctermfg=142 ctermbg=none guifg=#B8BB26 guibg=none
+  highlight SignifySignAdd ctermfg=142 ctermbg=none guifg=#B8BB26 guibg=none
+  highlight SignifyLineDelete guifg=#D82B26 guibg=none
+  highlight SignifySignDelete guifg=#D82B26 guibg=none
+]]
