189e2c4e2f2c45a03a482cd83b175640dd3014cc
max397574 <max.heinzer397@gmx.ch>
2021-08-17 21:21:43 +0200

    added file

---
+++ lua/colors/themes.lua
@@ -0,0 +1,77 @@
+Colo = {}
+
+local colors = {
+  'tokyonight',
+  'galaxy',
+  'moonlight',
+  'onedark',
+  'gruvbox8',
+  'tokyodark',
+  'gruvbox_material',
+}
+
+function Colo.random()
+	math.randomseed(os.clock())
+	local rand_int = math.random() * #colors
+	local selected = colors[math.floor(rand_int)+1]
+	print("ColorScheme: "..selected)
+  if selected == "color_galaxy" then
+    require "colors.color_galaxy".shine()
+    require "colors.vimcolors"
+    vim.cmd("highlight Normal guibg = none")
+  elseif selected == "gruvbox_material" then
+    vim.cmd("colorscheme gruvbox-material")
+  else
+    vim.cmd("colorscheme " .. selected)
+    require("colors.fixed_highlights")
+    vim.cmd("highlight Normal guibg = none")
+  end
+end
+
+
+function Colo.color_galaxy(transparent)
+  require "colors.color_galaxy".shine()
+  require "colors.vimcolors"
+  if transparent == true then
+    vim.cmd("highlight Normal guibg=none")
+  end
+end
+
+function Colo.moonlight(transparent)
+  vim.cmd("colorscheme moonlight")
+  if transparent == true then
+    vim.cmd("highlight Normal guibg=none")
+  end
+end
+function Colo.gruvbox8(transparent)
+  vim.cmd("colorscheme gruvbox8")
+  if transparent == true then
+    vim.cmd("highlight Normal guibg=none")
+  end
+end
+function Colo.onedark(transparent)
+  vim.cmd("colorscheme onedark")
+  if transparent == true then
+    vim.cmd("highlight Normal guibg=none")
+  end
+end
+function Colo.tokyonight(transparent)
+  vim.cmd("colorscheme tokyonight")
+  if transparent == true then
+    vim.cmd("highlight Normal guibg=none")
+  end
+end
+function Colo.tokyodark(transparent)
+  vim.cmd("colorscheme tokyodark")
+  if transparent == true then
+    vim.cmd("highlight Normal guibg=none")
+  end
+end
+function Colo.gruvbox_material(transparent)
+  vim.cmd("colorscheme gruvbox-material")
+  if transparent == true then
+    vim.cmd("highlight Normal guibg=none")
+  end
+end
+
+return Colo
