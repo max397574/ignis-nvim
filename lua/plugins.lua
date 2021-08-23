8d45b3e8a45930b732007588acbcaa6132e33144
max397574 <max.heinzer397@gmx.ch>
2021-08-23 16:07:18 +0200

    added neorg

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -3,6 +3,36 @@ local actions = require("telescope.actions")
 -- Plugins
 -- =======
 return require("packer").startup(function(use)
+  -- notes
+  use({
+    "vhyrro/neorg",
+    config = function()
+      require("neorg").setup({
+        -- Tell Neorg what modules to load
+        load = {
+          ["core.defaults"] = {}, -- Load all the default modules
+          ["core.norg.concealer"] = {}, -- Allows for use of icons
+          ["core.norg.dirman"] = { -- Manage your directories with Neorg
+            config = {
+              workspaces = {
+                my_workspace = "~/neorg",
+              },
+            },
+          },
+        },
+      })
+    end,
+    requires = "nvim-lua/plenary.nvim",
+  })
+  local parser_configs = require("nvim-treesitter.parsers").get_parser_configs()
+
+  parser_configs.norg = {
+    install_info = {
+      url = "https://github.com/vhyrro/tree-sitter-norg",
+      files = { "src/parser.c" },
+      branch = "main",
+    },
+  }
   -- package manager
   use("wbthomason/packer.nvim")
 
