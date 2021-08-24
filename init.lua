fbf3c18f8aaeb3b7990a4c004a0cb29eede0a080
max397574 <max.heinzer397@gmx.ch>
2021-08-24 16:06:30 +0200

    moved bufferline settings to plugins.lua

--- init.lua
+++ init.lua
@@ -27,36 +27,6 @@ vim.cmd("source ~/.config/nvim/random.vim")
 vim.cmd("colorscheme " .. color_choice)
 require("colors.fixed_highlights")
 vim.cmd("highlight Normal guibg = none")
-require("bufferline").setup({
-  options = {
-    custom_areas = {
-      right = function()
-        local result = {}
-        local error = vim.lsp.diagnostic.get_count(0, [[Error]])
-        local warning = vim.lsp.diagnostic.get_count(0, [[Warning]])
-        local info = vim.lsp.diagnostic.get_count(0, [[Information]])
-        local hint = vim.lsp.diagnostic.get_count(0, [[Hint]])
-
-        if error ~= 0 then
-          table.insert(result, { text = "  " .. error, guifg = "#EC5241" })
-        end
-
-        if warning ~= 0 then
-          table.insert(result, { text = "  " .. warning, guifg = "#EFB839" })
-        end
-
-        if hint ~= 0 then
-          table.insert(result, { text = "  " .. hint, guifg = "#A3BA5E" })
-        end
-
-        if info ~= 0 then
-          table.insert(result, { text = "  " .. info, guifg = "#7EA9A7" })
-        end
-        return result
-      end,
-    },
-  },
-})
 
 local time = os.date("*t")
 if time.hour >= 8 and time.hour < 20 then
