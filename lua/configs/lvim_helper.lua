1fcfb1d9f6dd3f14d0ee707559d62805aa71a228
max397574 <max.heinzer397@gmx.ch>
2021-09-07 15:24:57 +0200

    fixed filename

--- lua/configs/lvim_helper.lua
+++ lua/configs/lvim_helper.lua
@@ -6,7 +6,7 @@ require("lvim-helper").setup({
     home .. "/.config/nvim/vimhelp/ts_textobjects_select.md",
     home .. "/.config/nvim/vimhelp/telescope.md",
     home .. "/.config/nvim/vimhelp/cmp.md",
-    home .. "/.config/nvim/vimhelp/usefull.md",
+    home .. "/.config/nvim/vimhelp/useful.md",
   },
   winopts = {
     winhl = table.concat({ "Normal:LvimHelperNormal" }, ","),
