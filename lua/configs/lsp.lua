4d5729d55dbbccb3eccd0ab6e4374e8191e22a7a
max397574 <max.heinzer397@gmx.ch>
2021-09-08 21:25:29 +0200

    feat(lsp): ✨added highlighting

--- lua/configs/lsp.lua
+++ lua/configs/lsp.lua
@@ -1,4 +1,25 @@
 require("lspinstall").setup()
+local function lsp_highlight_document(client)
+  if client.resolved_capabilities.document_highlight == false then
+    return -- we don't need further
+  end
+  -- Set autocommands conditional on server_capabilities
+  if client.resolved_capabilities.document_highlight then
+    vim.api.nvim_exec(
+      [[
+      hi LspReferenceRead cterm=bold ctermbg=red guibg=#353d46
+      hi LspReferenceText cterm=bold ctermbg=red guibg=#353d46
+      hi LspReferenceWrite cterm=bold ctermbg=red guibg=#353d46
+      augroup lsp_document_highlight
+        autocmd! * <buffer>
+        autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
+        autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
+      augroup END
+    ]],
+      false
+    )
+  end
+end
 
 -- Refernce: https://github.com/kabouzeid/nvim-lspinstall/wiki
 local lua_settings = {
@@ -14,7 +35,9 @@ local function make_config()
   capabilities.textDocument.completion.completionItem.snippetSupport = true
   return {
     capabilities = capabilities,
-    on_attach = on_attach,
+    on_attach = function(client)
+      lsp_highlight_document(client)
+    end,
   }
 end
 
