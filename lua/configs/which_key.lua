31ba1bc6c120e5d35cdcfcd9241ecbc79c4135eb
max397574 <max.heinzer397@gmx.ch>
2021-09-20 15:02:51 +0200

    fix(treesitter): 🐛new mapping for usage

--- lua/configs/which_key.lua
+++ lua/configs/which_key.lua
@@ -7,13 +7,6 @@ require("which-key").setup {
 }
 
 wk.register({
-  n = { "TreeSitter Next Usage" },
-  p = { "TreeSitter Previous Usage" },
-}, {
-  prefix = "<leader>",
-  mode = "n",
-})
-wk.register({
   n = {
     name = "Goto Next, Incremental Selection, List",
     n = { "Incremental Selection" },
@@ -22,6 +15,7 @@ wk.register({
     O = { "List Definitions TOC" },
     ["0"] = { "List Lsp Workspace" },
     f = "Start Outer Function",
+    u = "Next Usage",
     i = {
       name = "Inner",
       c = { "Start Call" },
@@ -50,6 +44,7 @@ wk.register({
     c = { "Start Outer Call" },
     P = { "End Inner Parameter" },
     C = { "End Outer Call" },
+    u = { "Previous Usage"},
   },
 }, {
   prefix = "g",
