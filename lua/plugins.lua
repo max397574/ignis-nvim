727f7e05e886609b36fb542f001087c828a8334c
max397574 <max.heinzer397@gmx.ch>
2021-09-22 22:05:35 +0200

    feat(plugins): ✨lazyload better escape

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -26,6 +26,7 @@ require("packer").startup {
     -- better escape
     use {
       "~/betterEscape.nvim",
+      event = {"InsertEnter"},
       config = function()
         require("better_escape").setup()
       end,
