3d48624934a9b1f69d44063106781084b5d43be5
max397574 <max.heinzer397@gmx.ch>
2021-09-28 20:01:37 +0200

    feat(telescope): ✨fix for small screen

--- lua/configs/telescope.lua
+++ lua/configs/telescope.lua
@@ -2,6 +2,33 @@ local actions = require "telescope.actions"
 require("telescope").setup {
   defaults = {
     -- https://github.com/nvim-telescope/telescope.nvim/blob/master/lua/telescope/mappings.lua
+    layout_strategy = "horizontal",
+    layout_config = {
+      width = 0.90,
+      height = 0.85,
+      preview_cutoff = 90,
+      prompt_position = "bottom",
+      horizontal = {
+        preview_width = function(_, cols, _)
+          if cols > 200 then
+            return math.floor(cols * 0.5)
+          else
+            return math.floor(cols * 0.6)
+          end
+        end,
+      },
+      vertical = {
+        width = 0.9,
+        height = 0.95,
+        preview_height = 0.5,
+      },
+
+      flex = {
+        horizontal = {
+          preview_width = 0.9,
+        },
+      },
+    },
     mappings = {
       n = {
         ["<C-j>"] = actions.move_selection_next,
