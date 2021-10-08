5bdfa41f55067bce0a0549e2348da0742904dd3d
max397574 <max.heinzer397@gmx.ch>
2021-10-08 11:00:36 +0200

    feat(plugins): ✨updated startup config

--- lua/configs/startup_nvim.lua
+++ lua/configs/startup_nvim.lua
@@ -1,13 +1,25 @@
 local settings = {
   header = {
     type = "text",
-    content = require("startup.buildingblocks.headers").hydra(),
+    content = require("startup.buildingblocks.headers").neovim_logo(),
     align = "center",
+  },
+  body = {
+    type = "text",
+    -- content = require("startup.buildingblocks.functions").oldfiles(10),
+    content = { "" },
+    command = [[nnoremap <silent>I :e ~/.config/nvim/init.lua<CR>
+      nnoremap p <silent>:e ~/.config/nvim/lua/plugins.lua<CR>
+    ]],
   },
   footer = {
     type = "text",
-    content = require("startup.buildingblocks.functions").quote(),
+    content = require("startup.buildingblocks.functions").packer_plugins(),
     align = "center",
+  },
+  options = {
+    empty_lines_between_mappings = true,
+    mapping_keys = true,
   },
 }
 
