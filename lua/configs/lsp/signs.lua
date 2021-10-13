238d75b0eb4105b5ab98488b0fbe82bae30ef63f
max397574 <max.heinzer397@gmx.ch>
2021-10-13 17:59:15 +0200

    fix(diagnostics): 🐛use defined hl groups

--- lua/configs/lsp/signs.lua
+++ lua/configs/lsp/signs.lua
@@ -1,6 +1,6 @@
 vim.cmd [[
   sign define DiagnosticSignError text= texthl=DiagnosticError linehl= numhl=DiagnosticError
-  sign define DiagnosticSignWarn text= texthl=DiagnosticWarning linehl= numhl=DiagnosticWarning
-  sign define DiagnosticSignInfo text= texthl=DiagnosticInformation linehl= numhl=DiagnosticInformation
+  sign define DiagnosticSignWarn text= texthl=DiagnosticWarn linehl= numhl=DiagnosticWarn
+  sign define DiagnosticSignInfo text= texthl=DiagnosticInfo linehl= numhl=DiagnosticInfo
   sign define DiagnosticSignHint text= texthl=DiagnosticHint linehl= numhl=DiagnosticHint
 ]]
