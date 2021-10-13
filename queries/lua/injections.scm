a1c4aaf159ea312294e84dd7b174108c18c939d5
max397574 <max.heinzer397@gmx.ch>
2021-10-13 16:49:54 +0200

    feat(treesitter): ✨a test for vim injectin in lua

---
+++ queries/lua/injections.scm
@@ -0,0 +1,12 @@
+(
+  (function_call
+    (field_expression) @_vimcmd_identifier
+    (arguments (string) @vim)
+  )
+
+  (#eq? @_vimcmd_identifier "vim.cmd")
+  (
+    ; (#match? @vim "^\[\[") <- This is screwing stuff up
+    (#offset! @vim 0 2 0 -2)
+  )
+)
