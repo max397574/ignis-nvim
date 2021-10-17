3b63b87544ed212b92668ebb1385e71a74d1ba07
max397574 <max.heinzer397@gmx.ch>
2021-10-17 17:57:14 +0200

    cleanup(utils): 🗑️removed functions

--- lua/utils.lua
+++ lua/utils.lua
@@ -290,41 +290,19 @@ function M.lsp_config()
   dump(ret)
 end
 
--- TODO: Clean this mess with highest scope variables
--- TODO: extract this to folder
--- TODO: Create function to view files in float
-local win
-local help_file_nr = 1
-local function get_lines(file_number)
-  local files = require"configs.float_help"
-  if file_number > #files then
-    file_number = 1
-    help_file_nr = 1
-  elseif file_number < 1 then
-    file_number = #files
-    help_file_nr = #files
-  end
-  local lines_cat = vim.api.nvim_exec("!cat " .. files[help_file_nr], true)
-  local lines_lua = {}
-  for line in string.gmatch(lines_cat, "[^\n]+") do
-    table.insert(lines_lua,line)
-  end
-  table.remove(lines_lua, 1)
-  return lines_lua
-end
-
 function M.float_file(filepath)
   local lines_cat = vim.api.nvim_exec("!cat " .. filepath, true)
   local lines_lua = {}
   for line in string.gmatch(lines_cat, "[^\n]+") do
-    table.insert(lines_lua,line)
+    table.insert(lines_lua, line)
   end
   table.remove(lines_lua, 1)
   local buf = vim.api.nvim_create_buf(false, true)
-  vim.api.nvim_buf_set_lines(buf,0,-1,false,lines_lua)
+  vim.api.nvim_buf_set_lines(buf, 0, -1, false, lines_lua)
   local width = vim.api.nvim_win_get_width(0)
   local height = vim.api.nvim_win_get_height(0)
-  win = vim.api.nvim_open_win(buf, true, {relative = "win",
+  local win = vim.api.nvim_open_win(buf, true, {
+    relative = "win",
     win = 0,
     width = math.floor(width * 0.8),
     height = math.floor(height * 0.8),
@@ -333,8 +311,8 @@ function M.float_file(filepath)
     border = "single",
     style = "minimal",
   })
-  vim.api.nvim_win_set_option(win,"winblend",20)
-  vim.api.nvim_buf_set_option(buf,"modifiable",false)
+  vim.api.nvim_win_set_option(win, "winblend", 20)
+  vim.api.nvim_buf_set_option(buf, "modifiable", false)
 end
 
 return M
