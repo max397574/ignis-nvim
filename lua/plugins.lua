8fc744a21230529973943125a5263fb1fd993fba
max397574 <max.heinzer397@gmx.ch>
2021-10-25 22:43:12 +0200

    feat(plugins): ✨installed neorg

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -205,7 +205,7 @@ require("packer").startup {
     -- display keybindings help
     use {
       opt = true,
-      "max397574/which-key.nvim",
+      "folke/which-key.nvim",
       after = "nvim-treesitter",
       config = function()
         require("which-key").setup {}
@@ -215,6 +215,60 @@ require("packer").startup {
     }
 
     use "~/float_help.nvim/"
+
+    use "~/colorscheme_switcher/"
+
+    use {
+      "nvim-neorg/neorg",
+      after = "nvim-treesitter",
+      branch = "unstable",
+      config = function()
+        vim.cmd [[PackerLoad nvim-cmp]]
+        require("neorg").setup {
+          -- Tell Neorg what modules to load
+          load = {
+            ["core.defaults"] = {}, -- Load all the default modules
+            ["core.norg.concealer"] = {
+              config = {
+                icon_preset = "diamond",
+                icons = {
+                  marker = {
+                    icon = "",
+                  },
+                },
+              },
+            }, -- Allows for use of icons
+            ["core.keybinds"] = { -- Configure core.keybinds
+              config = {
+                default_keybinds = true, -- Generate the default keybinds
+                neorg_leader = "<Leader>o", -- This is the default if unspecified
+              },
+            },
+            ["core.norg.completion"] = {
+              config = {
+                engine = "nvim-cmp", -- We current support nvim-compe and nvim-cmp only
+              },
+            },
+            ["core.norg.dirman"] = { -- Manage your directories with Neorg
+              config = {
+                workspaces = {
+                  my_workspace = "~/neorg",
+                  startup = "~/startup.nvim",
+                },
+              },
+            },
+            ["core.gtd.base"] = {
+              config = {
+                workspace = "startup", -- assign the workspace,
+                -- exclude = { "file_to_exclude.norg" } -- Optional: all excluded files from the workspace are not part of the gtd workflow
+              },
+            },
+          },
+        }
+      end,
+
+      requires = "nvim-lua/plenary.nvim",
+    }
 
     use {
       "jameshiew/nvim-magic",
