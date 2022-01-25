8b7f801b62166cb9a0f532ed5b65b316c5745b0a
max397574 <max.heinzer397@gmx.ch>
2022-01-25 21:37:52 +0100

    feat(treesitter): don't highlight usages(done by lsp)

--- lua/configs/treesitter.lua
+++ lua/configs/treesitter.lua
@@ -65,7 +65,7 @@ require("nvim-treesitter.configs").setup({
         },
     },
     refactor = {
-        highlight_definitions = { enable = true },
+        highlight_definitions = { enable = false },
         highlight_current_scope = { enable = false },
         smart_rename = {
             enable = true,
