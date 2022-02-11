1fcc94b4f77accb05822fddef328c7f75460f4d6
max397574 <max.heinzer397@gmx.ch>
2022-02-11 13:47:53 +0100

    feat(autopairs): get rid of space around =

--- lua/configs/nvim_autopairs.lua
+++ lua/configs/nvim_autopairs.lua
@@ -22,33 +22,33 @@ require("nvim-autopairs").setup({
 -- npairs.add_rule(Rule("~","|","norg"))
 
 npairs.add_rules({
-    Rule("=", "")
-        :with_pair(cond.not_inside_quote())
-        :with_pair(function(opts)
-            local last_char = opts.line:sub(opts.col - 1, opts.col - 1)
-            if last_char:match("[%w%=%s]") then
-                return true
-            end
-            return false
-        end)
-        :replace_endpair(function(opts)
-            local prev_2char = opts.line:sub(opts.col - 2, opts.col - 1)
-            local next_char = opts.line:sub(opts.col, opts.col)
-            next_char = next_char == " " and "" or " "
-            if prev_2char:match("%w$") then
-                return "<bs> =" .. next_char
-            end
-            if prev_2char:match("%=$") then
-                return next_char
-            end
-            if prev_2char:match("=") then
-                return "<bs><bs>=" .. next_char
-            end
-            return ""
-        end)
-        :set_end_pair_length(0)
-        :with_move(cond.none())
-        :with_del(cond.none()),
+    -- Rule("=", "")
+    --     :with_pair(cond.not_inside_quote())
+    --     :with_pair(function(opts)
+    --         local last_char = opts.line:sub(opts.col - 1, opts.col - 1)
+    --         if last_char:match("[%w%=%s]") then
+    --             return true
+    --         end
+    --         return false
+    --     end)
+    --     :replace_endpair(function(opts)
+    --         local prev_2char = opts.line:sub(opts.col - 2, opts.col - 1)
+    --         local next_char = opts.line:sub(opts.col, opts.col)
+    --         next_char = next_char == " " and "" or " "
+    --         if prev_2char:match("%w$") then
+    --             return "<bs> =" .. next_char
+    --         end
+    --         if prev_2char:match("%=$") then
+    --             return next_char
+    --         end
+    --         if prev_2char:match("=") then
+    --             return "<bs><bs>=" .. next_char
+    --         end
+    --         return ""
+    --     end)
+    --     :set_end_pair_length(0)
+    --     :with_move(cond.none())
+    --     :with_del(cond.none()),
     Rule(" ", " "):with_pair(function(opts)
         local pair = opts.line:sub(opts.col - 1, opts.col)
         return vim.tbl_contains({ "()", "[]", "{}" }, pair)
