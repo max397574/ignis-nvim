ed114d19ca82c9e97c8986991e599af0147216cb
max397574 <max.heinzer397@gmx.ch>
2022-02-22 15:36:37 +0100

    feat(neorg): renabled ftplugin

--- ftplugin/norg.lua
+++ ftplugin/norg.lua
@@ -2,44 +2,50 @@ vim.bo.shiftwidth = 2
 vim.o.conceallevel = 2
 vim.bo.commentstring = "#%s"
 
--- local wk = require("which-key")
---
--- wk.register({
---     t = {
---         name = "+Tasks",
---         d = { "Done" },
---         u = { "Undone" },
---         p = { "Pending" },
---         i = { "Important" },
---         h = { "On Hold" },
---         c = { "Cancelled" },
---         r = { "Recurring" },
---     },
--- }, {
---     prefix = "g",
---     mode = "n",
--- })
---
--- wk.register({
---     m = {
---         name = "+Mode",
---         n = "Norg",
---         h = "Traverse Heading",
---     },
---     t = {
---         name = "+Gtd",
---         c = "Capture",
---         e = "Edit",
---         v = "Views",
---     },
---     n = {
---         name = "+New Note",
---         n = "New Note",
---     },
---     l = { "<cmd>Telescope neorg insert_link<CR>", "Insert Link" },
--- }, {
---     prefix = "<leader>o",
---     mode = "n",
--- })
---
--- vim.cmd([[source ~/.config/nvim_config/clipboard_neorg.vim]])
+local system = require("ignis.utils.system")
+
+local wk = require("which-key")
+
+wk.register({
+    t = {
+        name = "+Tasks",
+        d = { "Done" },
+        u = { "Undone" },
+        p = { "Pending" },
+        i = { "Important" },
+        h = { "On Hold" },
+        c = { "Cancelled" },
+        r = { "Recurring" },
+    },
+}, {
+    prefix = "g",
+    mode = "n",
+})
+
+wk.register({
+    m = {
+        name = "+Mode",
+        n = "Norg",
+        h = "Traverse Heading",
+    },
+    t = {
+        name = "+Gtd",
+        c = "Capture",
+        e = "Edit",
+        v = "Views",
+    },
+    n = {
+        name = "+New Note",
+        n = "New Note",
+    },
+    l = { "<cmd>Telescope neorg insert_link<CR>", "Insert Link" },
+}, {
+    prefix = "<leader>o",
+    mode = "n",
+})
+
+vim.cmd(
+    "source " .. system.ignis_dir .. system.separator .. "clipboard_neorg.vim"
+)
+
+vim.cmd([[hi link NeorgMarkupVerbatim Comment]])
