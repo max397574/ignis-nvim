36a908a69f4a0daaa651ea6714e70040576c1e74
max397574 <max.heinzer397@gmx.ch>
2022-02-22 16:39:04 +0100

    feat(db): added telescope layout

--- lua/custom/db.lua
+++ lua/custom/db.lua
@@ -4,6 +4,8 @@ local db = sql:open(vim.fn.stdpath("data") .. "/databases/colorscheme.db") -- op
 
 local cs_tbl = db:tbl("colorscheme", { name = "text" })
 
+local ts_tbl = db:tbl("confi", { layout = "bottom" })
+
 local M = {}
 
 function M.set_db()
@@ -11,9 +13,23 @@ function M.set_db()
     cs_tbl:insert({ name = vim.g.colors_name })
 end
 
+function M.change_ts_layout(layout)
+    ts_tbl:drop()
+    ts_tbl:insert({ layout = layout })
+    RELOAD("ignis.modules.files.telescope")
+    -- require("colors").init()
+    RELOAD("colors.highlights")
+    require("colors.highlights")
+end
+
 function M.get_scheme()
     local tbl = cs_tbl:get()
     return tbl[1]["name"]
+end
+
+function M.get_ts_layout()
+    local tbl = ts_tbl:get()
+    return tbl[1]["layout"]
 end
 
 return M
