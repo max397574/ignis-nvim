120fef8bc51c84ff83851eafa0fa6b45f82075f1
max397574 <max.heinzer397@gmx.ch>
2022-02-22 16:42:25 +0100

    cleanup(colors): remove old function

--- lua/colors/init.lua
+++ lua/colors/init.lua
@@ -1,31 +1,14 @@
+-- code from https://github.com/NvChad/NvChad
 local colors = {}
-
-local function highlight(group, guifg, guibg, attr, guisp)
-    local parts = { group }
-    if guifg then
-        table.insert(parts, "guifg=#" .. guifg)
-    end
-    if guibg then
-        table.insert(parts, "guibg=#" .. guibg)
-    end
-    if attr then
-        table.insert(parts, "gui=" .. attr)
-    end
-    if guisp then
-        table.insert(parts, "guisp=#" .. guisp)
-    end
-
-    -- nvim.ex.highlight(parts)
-    vim.api.nvim_command("highlight " .. table.concat(parts, " "))
-end
 
 -- if theme given, load given theme if given, otherwise nvchad_theme
 colors.init = function(theme)
-    -- if not theme then
-    --   theme = "onedark"
-    -- end
-
     -- set the global theme, used at various places like theme switcher, highlights
+    if not theme then
+        if vim.g.colors_name then
+            theme = vim.g.colors_name
+        end
+    end
     vim.g.nvchad_theme = theme
     vim.g.colors_name = theme
 
