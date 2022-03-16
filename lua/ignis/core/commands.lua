f56844e7451514fe867408b7ae5297545ca113e1
max397574 <max.heinzer397@gmx.ch>
2022-03-16 21:07:37 +0100

    fix(core.commands): use correct path for require

--- lua/ignis/core/commands.lua
+++ lua/ignis/core/commands.lua
@@ -1,7 +1,7 @@
 local add_cmd = vim.api.nvim_add_user_command
 
 add_cmd("ViewColors", function()
-    require("lua.colors.color_viewer").view_colors()
+    require("colors.color_viewer").view_colors()
 end, {})
 
 add_cmd("ReloadColors", function()
