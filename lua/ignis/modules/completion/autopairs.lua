b2eb1da3d386591b1271b67f6651cc0fde2c97e6
max397574 <max.heinzer397@gmx.ch>
2022-03-25 20:40:51 +0100

    feat(completion): improved autopairs

--- lua/ignis/modules/completion/autopairs.lua
+++ lua/ignis/modules/completion/autopairs.lua
@@ -1,4 +1,8 @@
 local Rule = require("nvim-autopairs.rule")
 local npairs = require("nvim-autopairs")
-require("nvim-autopairs").setup({})
+require("nvim-autopairs").setup({
+    -- ignored_next_char = string.gsub([[ [%w%%%'%[%"%.] ]], "%s+", ""),
+    ignored_next_char = "",
+    map_c_w = true,
+})
 npairs.add_rule(Rule("$", "$", "tex"))
