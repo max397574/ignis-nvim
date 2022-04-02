5585af471cf78b799221836d7ab6cafb39c950a1
max397574 <max.heinzer397@gmx.ch>
2022-04-02 18:25:36 +0200

    feat(modules): added some new plugins

--- lua/ignis/modules/init.lua
+++ lua/ignis/modules/init.lua
@@ -135,7 +135,6 @@ packer.startup({
 
         use({ "VonHeikemen/searchbox.nvim", opt = true })
         use({ "elihunter173/dirbuf.nvim", opt = true })
-        use({ "mizlan/iswap.nvim", opt = true })
         use({ "lervag/vimtex", config = function() end, filetype = "tex" })
         use({ "~/neovim_plugins/nabla.nvim", filetype = "tex" })
         -- use({ "~/neovim_plugins/lambda.nvim", filetype = "tex" })
@@ -143,6 +142,17 @@ packer.startup({
         use({
             "~/neovim_plugins/selection_popup",
             module = "selection_popup",
+        })
+
+        use({
+            "mizlan/iswap.nvim",
+            cmd = { "ISwap", "ISwapWith" },
+            config = function()
+                require("iswap").setup({
+                    keys = "qwertzuiop",
+                    autoswap = true,
+                })
+            end,
         })
 
         use({
@@ -408,8 +418,68 @@ packer.startup({
                     auto_open = 1,
                     auto_close = 1,
                     follow = 1,
+                    update_to_buf_dir = { enable = false },
                     -- disable_netrw = 0,
                 })
+            end,
+        })
+
+        use({
+            "hrsh7th/vim-searchx",
+            opt = true,
+            setup = function()
+                -- Overwrite / and ?.
+                vim.keymap.set({ "n", "x" }, "?", function()
+                    vim.cmd("PackerLoad vim-searchx")
+                    vim.cmd("call searchx#start({ 'dir': 0 })")
+                end)
+                vim.keymap.set({ "n", "x" }, "/", function()
+                    vim.cmd("PackerLoad vim-searchx")
+                    vim.cmd("call searchx#start({ 'dir': 1 })")
+                end)
+                -- Move to next/prev match.
+                vim.keymap.set(
+                    { "n", "x" },
+                    "N",
+                    "<Cmd>call searchx#prev_dir()<CR>"
+                )
+                vim.keymap.set(
+                    { "n", "x" },
+                    "n",
+                    "<Cmd>call searchx#next_dir()<CR>"
+                )
+
+                -- Clear highlights
+                vim.keymap.set("n", "<Esc>", "<Cmd>call searchx#clear()<CR>)")
+            end,
+            config = function()
+                vim.api.nvim_exec(
+                    [=[
+                        let g:searchx = {}
+                        " Auto jump if the recent input matches to any marker.
+                        let g:searchx.auto_accept = v:true
+                        " The scrolloff value for moving to next/prev.
+                        let g:searchx.scrolloff = &scrolloff
+                        " To enable scrolling animation.
+                        let g:searchx.scrolltime = 500
+                        " To enable auto nohlsearch after cursor is moved
+                        let g:searchx.nohlsearch = {}
+                        let g:searchx.nohlsearch.jump = v:true
+                        " Marker characters.
+                        let g:searchx.markers = split('ABCDEFGHIJKLMNOPQRSTUVWXYZ', '.\zs')
+                        " Convert search pattern.
+                        function g:searchx.convert(input) abort
+                          if a:input !~# '\k'
+                            return '\V' .. a:input
+                          endif
+                          return a:input[0] .. substitute(a:input[1:], '\\\@<! ', '.\\{-}', 'g')
+                        endfunction
+                        " Set highlight for markers
+                        highlight! link SearchxMarker DiffChange
+                        highlight! link SearchxMarkerCurrent WarningMsg
+                    ]=],
+                    false
+                )
             end,
         })
 
