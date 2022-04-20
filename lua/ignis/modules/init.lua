52bf258ee4db544e8ca644a247a6bcd17572e1dd
max397574 <max.heinzer397@gmx.ch>
2022-04-20 17:20:44 +0200

    feat(lsp): better sign for ligthbulb

--- lua/ignis/modules/init.lua
+++ lua/ignis/modules/init.lua
@@ -811,6 +811,10 @@ packer.startup({
                 vim.cmd(
                     [[autocmd CursorHold,CursorHoldI * lua require'nvim-lightbulb'.update_lightbulb()]]
                 )
+                vim.fn.sign_define(
+                    "LightBulbSign",
+                    { text = " ", texthl = "String", linehl = "", numhl = "" }
+                )
             end,
         })
 
