0308fcffcd3fc955b80c2f22b8144aac7a2d5a1d
max397574 <max.heinzer397@gmx.ch>
2022-04-27 10:21:40 +0200

    feat(db): add heirline

--- lua/custom/db.lua
+++ lua/custom/db.lua
@@ -8,18 +8,28 @@ local ts_tbl = tbl("ts_tbl", {
     layout = "text",
 }, db)
 
+local heirline_tbl = tbl("ts_tbl", {
+    key = { "text", primary = true, required = true, default = "none" },
+    theme = "text",
+}, db)
+
 local cs_tbl = tbl("cs_tbl", {
     key = { "text", primary = true, required = true, default = "none" },
     name = "text",
 }, db)
 
 local M = {}
+-- ts_tbl.tele = {}
+-- cs_tbl.scheme = {}
+-- heirline_tbl.heirline = {}
 
 function M.set_db()
     cs_tbl.scheme.name = vim.g.colors_name
 end
 
+-- BUG: Completely broken
 function M.change_ts_layout(layout)
+    ts_tbl.tele = {}
     ts_tbl.tele.layout = layout
     RELOAD("ignis.modules.files.telescope")
     -- require("colors").init()
@@ -27,12 +37,27 @@ function M.change_ts_layout(layout)
     require("colors.highlights")
 end
 
+function M.get_ts_layout()
+    return "custom_bottom_no_borders"
+    -- return "float_all_borders"
+    -- return "float_prompt_border"
+end
+
 function M.get_scheme()
     return cs_tbl.scheme.name
 end
 
-function M.get_ts_layout()
-    return ts_tbl.tele.layout
+function M.change_heirline_theme(theme)
+    heirline_tbl.heirline.theme = theme
+    RELOAD("ignis.modules.ui.heirline")
+    require("ignis.modules.ui.heirline")
+    -- require("colors").init()
+    -- RELOAD("colors.highlights")
+    require("colors.highlights")
+end
+
+function M.get_heirline_theme()
+    return heirline_tbl.heirline.theme
 end
 
 return M
