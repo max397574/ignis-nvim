e49d75ae2a947298832f15503ada8a5cc0e69e7c
max397574 <max.heinzer397@gmx.ch>
2022-05-01 12:52:40 +0200

    feat(autocommands): notify when neorg is loaded

--- lua/ignis/core/autocommands.lua
+++ lua/ignis/core/autocommands.lua
@@ -249,3 +249,16 @@ aucmd("DiagnosticChanged", {
     end,
     desc = "Send desktop notification",
 })
+
+aucmd("User", {
+    once = true,
+    callback = function()
+        vim.api.nvim_chan_send(vim.v.stderr, "\027]99;i=1:d=0;Neorg\027\\")
+        vim.api.nvim_chan_send(
+            vim.v.stderr,
+            "\027]99;i=1:d=1:p=body;Finished Loading\027\\"
+        )
+    end,
+    pattern = "NeorgStarted",
+    desc = "Send desktop notification",
+})
