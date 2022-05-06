e3966aabb27e10de421188442dffe82668419d6f
max397574 <max.heinzer397@gmx.ch>
2022-05-06 18:31:11 +0200

    fix(telescope): don't color md files preview (segfault)

--- lua/ignis/modules/files/telescope/init.lua
+++ lua/ignis/modules/files/telescope/init.lua
@@ -467,6 +467,30 @@ end
 
 require("telescope").setup(configs)
 
+local _bad = { ".*%.md" } -- Put all filetypes that slow you down in this array
+local bad_files = function(filepath)
+  for _, v in ipairs(_bad) do
+    if filepath:match(v) then
+      return false
+    end
+  end
+
+  return true
+end
+
+local new_maker = function(filepath, bufnr, opts)
+  opts = opts or {}
+  if opts.use_ft_detect == nil then opts.use_ft_detect = true end
+  opts.use_ft_detect = opts.use_ft_detect == false and false or bad_files(filepath)
+  previewers.buffer_previewer_maker(filepath, bufnr, opts)
+end
+
+require("telescope").setup {
+  defaults = {
+    buffer_previewer_maker = new_maker,
+  }
+}
+
 function ts.entry_preview(contents, opts)
     require("hubble").entry_preview(contents, opts)
 end
@@ -601,6 +625,7 @@ function ts.find_string()
             "%.svg",
             "%.otf",
             "%.ttf",
+            -- "%.md" -- gives segfaults
         },
         preview = {
             hide_on_startup = false,
