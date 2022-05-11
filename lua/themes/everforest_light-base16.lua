e470841e42aa53bd6cd6eef51ba7f821f3680137
max397574 <max.heinzer397@gmx.ch>
2022-05-11 09:32:40 +0200

    feat(themes): added some new ones

---
+++ lua/themes/everforest_light-base16.lua
@@ -0,0 +1,18 @@
+return {
+   base00 = "#fff9e8",
+   base01 = "#f7f4e0",
+   base02 = "#f0eed9",
+   base03 = "#868d80",
+   base04 = "#d3c6aa",
+   base05 = "#272f35",
+   base06 = "#e9e8d2",
+   base07 = "#fff9e8",
+   base08 = "#5f9b93",
+   base09 = "#b67996",
+   base0A = "#8da101",
+   base0B = "#dfa000",
+   base0C = "#f57d26",
+   base0D = "#87a060",
+   base0E = "#f85552",
+   base0F = "#d699b6",
+}
